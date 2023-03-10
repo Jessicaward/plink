c24c1aa81474bb0a75655cd2c03f64cf79704e41
Jessica Ward <jessicawardiom@gmail.com>
2023-03-10 19:56:29 +0000

    Added depth and unique checks to analyser

--- plink/analyser.py
+++ plink/analyser.py
@@ -24,19 +24,23 @@ class Analyser():
         try:
             content = self.retrieve_content_by_url(url)
             links = self.retrieve_links_from_html(content, url)
-            print(colored((depth * "\t") + "Success: " + url, "green"))
+            print(colored((depth * "  ") + "Success: " + url, "green"))
             return Result(links=links, status="Success")
         except Exception as ex:
             if self.options.verbose:
-                print(colored(str(ex), "red"))
-            print(colored((depth * "\t") + "Fail: " + url, "red"))
+                print(colored((depth * "  ") + str(ex), "red"))
+            print(colored((depth * "  ") + "Fail: " + url, "red"))
             return Result(status="Fail")
 
     def analyse(self):
         analysed_urls = []
         urls_to_analyse = [self.options.start_url]
         for depth in range(0, self.options.recursion_depth):
-            for url in urls_to_analyse:
+            urls_in_step = urls_to_analyse[:] # Copy list without reference
+            for url in urls_in_step:
                 result = self.analyse_url(url, depth)
-                analysed_urls.append(analysed_urls)
+                analysed_urls.append(url)
                 urls_to_analyse.remove(url)
+                for url_to_analyse in result.links:
+                    if url_to_analyse not in urls_to_analyse:
+                        urls_to_analyse.append(url_to_analyse)
