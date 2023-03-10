39f983bfd23851f23ff6f226d6d1a592b7594918
Jessica Ward <jessicawardiom@gmail.com>
2023-03-10 19:05:47 +0000

    Added content and link retrieval

--- plink/analyser.py
+++ plink/analyser.py
@@ -1,3 +1,19 @@
+import requests
+from bs4 import BeautifulSoup
+
+def retrieve_content_by_url(url):
+    request = requests.get(url)
+    content = request.text
+    return content
+
+def retrieve_links_from_html(html):
+    soup = BeautifulSoup(html, 'html')
+    link_tags = soup.find_all('a')
+    links = [tag['href'] for tag in link_tags]
+    print(links)
+
 def analyse(options):
     print("Analysing")
     print(options.start_url)
+    content = retrieve_content_by_url(options.start_url)
+    retrieve_links_from_html(content)
